09d426189b92e5b59c2dea9f1078690aa86fca7c
hieu <hieushinnosuke@gmail.com>
2024-08-29 03:14:32 +0800

    fixed and tested

--- api/database/database.go
+++ api/database/database.go
@@ -1,18 +1,21 @@
 package database
-
 import (
 	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
-	"os"
 )
-
 var Ctx = context.Background()
-
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASS"),
+		Addr:     "redis-15884.c10.us-east-1-4.ec2.redns.redis-cloud.com:15884",
+		Password: "",
 		DB:       dbNo,
 	})
+	_, err := rdb.Ping(Ctx).Result()
+	if err != nil {
+		fmt.Printf("Could not connect to Redis: %v\n", err)
+		return nil
+	}
+	fmt.Println("Successfully connected to Redis")
 	return rdb
 }
